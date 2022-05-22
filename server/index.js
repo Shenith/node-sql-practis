402e645473734933a4ff6351bc6e7789bbc273ce
Shenith <shenithsandeepa@gmail.com>
2022-05-22 23:58:19 +0530

    first day post req

--- server/index.js
+++ server/index.js
@@ -1,34 +1,37 @@
 const express = require("express");
 const app = express();
 const mysql = require("mysql");
+const cors = require("cors");
+
+app.use(cors());
+app.use(express.json());
 
 const db = mysql.createConnection({
 	user: "root",
 	host: "localhost",
-	password: "password",
+	password: "Astro@0312260057",
 	database: "employeeSystem",
 });
 
 app.post("/create", (req, res) => {
 	console.log(req.body);
-	// const name = req.body.name;
-	// const age = req.body.age;
-	// const country = req.body.country;
-	// const position = req.body.position;
-	// const wage = req.body.wage;
+	const name = req.body.name;
+	const age = req.body.age;
+	const country = req.body.country;
+	const position = req.body.position;
+	const wage = req.body.wage;
 
-	// db.query(
-	// 	"INSERT INTO employee (name, age, country, position, wage) VALUES (?,?,?,?,?)",
-	// 	[name, age, country, position, wage],
-	// 	(err, result) => {
-	// 		if (err) {
-	// 			console.log(err);
-	// 		} else {
-	// 			res.send("Values inserted");
-	// 		}
-	// 	}
-	// );
-	res.send("Values inserted");
+	db.query(
+		"INSERT INTO employee (name, age, country, position, wage) VALUES (?,?,?,?,?)",
+		[name, age, country, position, wage],
+		(err, result) => {
+			if (err) {
+				console.log(err);
+			} else {
+				res.send("Values inserted");
+			}
+		}
+	);
 });
 
 app.listen(3001, () => {
